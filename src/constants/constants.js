1815c3dd5cd119ca667b7ff30a5b8037a9ea15ec
Linh Vu <linh777gkog@gmail.com>
2022-09-13 20:17:57 +0700

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'Crypto Tracker',
     description:
       'This is a crypto currency tracker app that I built with React. It uses the CoinGecko API to fetch the data. I used React Hooks and Context API to manage the state of the app.',
-    image: '/images/3.png',
+    image: '/images/3.PNG',
     tags: ['React', 'Tailwind CSS', 'Firebase', 'React Hooks'],
     source: 'https://github.com/Linh777GKOG/crypto-app',
     visit: 'https://crypto-project-app.vercel.app/',
@@ -13,7 +13,7 @@ export const projects = [
     title: 'E-Commerce',
     description:
       'This is an e-commerce app that I built with Next.js. It uses the Stripe API to process payments, include modern design and animations, the ability to add and edit products on the go using a Sanity.',
-    image: '/images/headphone.png',
+    image: '/images/headphone.PNG',
     tags: ['React', 'Stripe', 'NextJs', 'Sanity'],
     source: 'https://github.com/Linh777GKOG/SS-E-Commercep',
     visit: 'https://ss-e-commerce.vercel.app/',
@@ -23,7 +23,7 @@ export const projects = [
     title: 'Movie App',
     description:
       'This is a movie app that I built with React,Tailwind CSS, TypeScript and NextJs. It uses the TMDB API to fetch the data. The data is fetched from The Movie DB Api.',
-    image: '/images/1.png',
+    image: '/images/1.PNG',
     tags: ['ReactJS', 'NextJS', 'Tailwind CSS', 'TypeScript'],
     source: 'https://github.com/Linh777GKOG/movie_app',
     visit: 'https://movie-webapp-rmdb.vercel.app/',
@@ -33,7 +33,7 @@ export const projects = [
     title: 'This Website',
     description:
       'This is my personal website that I built with React, Next JS, and deployed on Vercel, a cloud platform for static sites, APIs, and serverless functions, developed by Vercel Inc.',
-    image: '/images/Portfolio.png',
+    image: '/images/Portfolio.PNG',
     tags: ['React', 'JavaScript', 'NextJS'],
     source: 'https://github.com/Linh777GKOG/Portfolio_nextjs',
     visit: 'https://linh777.vercel.app/',
@@ -43,7 +43,7 @@ export const projects = [
     title: "Bob's A Wuss",
     description:
       "Bob's a Wuss - an endless runner build with pure javascript, HTML5 Canvas for rendering and React to handle the UI.",
-    image: '/images/Start.png',
+    image: '/images/Start.PNG',
     tags: ['React', 'JavaScript', 'HTML5'],
     source: 'https://github.com/Linh777GKOG/Bobawuss',
     visit: 'https://bobawuss.vercel.app/',
@@ -53,7 +53,7 @@ export const projects = [
     title: 'Food Delivery',
     description:
       'This is a full stack food delivery application using Reactjs, Firebase, Framer motion, Tailwind css, allow users to add new items and maintain over firestore cloud database. ',
-    image: '/images/22.png',
+    image: '/images/22.PNG',
     tags: ['React', 'Redux', 'Firebase', 'Tailwind CSS'],
     source: 'https://github.com/Linh777GKOG/Foodapp',
     visit: 'https://restaurant-app-ce761.web.app/',
